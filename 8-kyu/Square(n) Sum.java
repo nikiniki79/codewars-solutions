87eeb3a7cfbe89806dd0c43fe35b2d423feac7df
nikiniki79 <75273923+nikiniki79@users.noreply.github.com>
2022-07-20 18:00:28 +0300

    Update Square(n) Sum.java

--- 8-kyu/Square(n) Sum.java
+++ 8-kyu/Square(n) Sum.java
@@ -1,7 +1,5 @@
-public class Kata
- {
-  public static int squareSum(int[] n)
-  { 
+public class Kata {
+  public static int squareSum(int[] n) { 
     int sum = 0;
     for (int i = 0; i < n.length; i++) {
       sum += n[i] * n[i];
